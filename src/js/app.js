df139dceee8d9cd257103f1e14e4e707fe1e6972
tamuk <tamuka120>
2018-10-21 04:52:54 +0100

    Blockchain id

---
+++ src/js/app.js
@@ -0,0 +1,87 @@
+App = {
+web3Provider: null,
+contracts: {},
+account: '0x0',
+
+init: function() {
+  return App.initWeb3();
+},
+
+initWeb3: function() {
+  if (typeof web3 !== 'undefined') {
+    // If a web3 instance is already provided by Meta Mask.
+    App.web3Provider = web3.currentProvider;
+    web3 = new Web3(web3.currentProvider);
+  } else {
+    // Specify default instance if no web3 instance provided
+    App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+    web3 = new Web3(App.web3Provider);
+  }
+  return App.initContract();
+},
+
+initContract: function() {
+  $.getJSON("UserID.json", function(election) {
+    // Instantiate a new truffle contract from the artifact
+    App.contracts.UserID = TruffleContract(election);
+    // Connect provider to interact with contract
+    App.contracts.UserID.setProvider(App.web3Provider);
+
+    return App.render();
+  });
+},
+
+render: function() {
+  var userInstance;
+  var loader = $("#loader");
+  var content = $("#content");
+
+  loader.show();
+  content.hide();
+
+  // Load account data
+  web3.eth.getCoinbase(function(err, account) {
+    if (err === null) {
+      App.account = account;
+      $("#accountAddress").html("Your Account: " + account);
+    }
+  });
+
+  // Load contract data
+  App.contracts.UserID.deployed().then(function(instance) {
+    userInstance = instance;
+    return userInstance.UID_Count();
+  }).then(function(count) {
+    var candidatesResults = $("#candidatesResults");
+    var nameResults = $("#user_name");
+    candidatesResults.empty();
+
+    for (var i = 1; i <= count; i++) {
+      userInstance.UID_Map(i).then(function(map) {
+        var id = map[0];
+        var name = map[1];
+        var dob = map[2];
+        var exp_date = map[3];
+        var infractions = map[4];
+        var passportId = map[5];
+
+        // Render candidate Result
+        var candidateTemplate = "<tr><th>" + id + "</th><td>" + name + "</td><td>" + dob + "</td><td>" + exp_date + "</td><td>" + infractions + "</td><td>" + passportId + "</td></tr>";
+        candidatesResults.append(candidateTemplate);
+        document.getElementById("userResults").innerHTML = name;
+      });
+    }
+
+    loader.hide();
+    content.show();
+  }).catch(function(error) {
+    console.warn(error);
+  });
+}
+};
+
+$(function() {
+$(window).load(function() {
+  App.init();
+});
+});
